8b11308a87bf586ebedd500f1c1b51b31f01ac41
Azhar Hayat <115901212+Azharhayat271@users.noreply.github.com>
2024-09-07 02:55:41 -0700

    final commit

--- frontend/src/layout/topbar.jsx
+++ frontend/src/layout/topbar.jsx
@@ -44,45 +44,7 @@ const Topbar = ({ toggleActive, toggleMobileActive }) => {
           </div>
           <div className="col-auto">
             <div className="d-flex flex-wrap align-items-center gap-3">
-              <div className="dropdown">
-                <button
-                  className="has-indicator w-40-px h-40-px bg-neutral-200 rounded-circle d-flex justify-content-center align-items-center"
-                  type="button"
-                  id="mailDropdown"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    src={Mail}
-                    className="text-primary-light text-xl"
-                    style={{ width: "60%" }}
-                    alt="Mail"
-                  />
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="mailDropdown">
-                  {/* Mail dropdown content (if any) */}
-                </ul>
-              </div>
-
-              <div className="dropdown">
-                <button
-                  className="has-indicator w-40-px h-40-px bg-neutral-200 rounded-circle d-flex justify-content-center align-items-center"
-                  type="button"
-                  id="notificationDropdown"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    src={Notification}
-                    className="text-primary-light text-xl"
-                    style={{ width: "70%" }}
-                    alt="Notification"
-                  />
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="notificationDropdown">
-                  {/* Notification dropdown content (if any) */}
-                </ul>
-              </div>
+           
 
               <div className="dropdown">
                 <button
